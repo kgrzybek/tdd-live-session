003117f86d79e79cf285cb93b2086c8bf8125f15
Kamil Grzybek <kamgrzybek@gmail.com>
2020-05-23 20:44:53 +0200

    First iteration - Add SalesCalculator and first implementation

---
+++ src/TDD.LiveSession/TDD.LiveSession.Domain.UnitTests/SalesCalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TDD.LiveSession.Domain.UnitTests
+{
+    [TestFixture]
+    public class SalesCalculatorTests
+    {
+        [Test]
+        public void SalesCalculator_Calculate_Test()
+        {
+            decimal salesValue = 1000;
+            var points = SalesCalculator.Calculate(salesValue);
+
+            Assert.That(points, Is.EqualTo(10));
+        }
+    }
+
+    public class SalesCalculator
+    {
+        public static decimal Calculate(decimal salesValue)
+        {
+            return salesValue / 100;
+        }
+    }
+}
