ffc943dc24924c5fcbb76fe55f005903b145ffc5
Kamil Grzybek <kamgrzybek@gmail.com>
2020-05-23 21:07:34 +0200

    2 iteration - Add MoneyValue

--- src/TDD.LiveSession/TDD.LiveSession.Domain.UnitTests/SalesCalculatorTests.cs
+++ src/TDD.LiveSession/TDD.LiveSession.Domain.UnitTests/SalesCalculatorTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Reflection.Emit;
+using NUnit.Framework;
 
 namespace TDD.LiveSession.Domain.UnitTests
 {
@@ -8,16 +10,36 @@ namespace TDD.LiveSession.Domain.UnitTests
         [Test]
         public void SalesCalculator_Calculate_Test()
         {
-            decimal salesValue = 1000;
+            MoneyValue salesValue = MoneyValue.Of(1000);
             var points = SalesCalculator.Calculate(salesValue);
 
             Assert.That(points, Is.EqualTo(10));
         }
     }
 
+    public struct MoneyValue
+    {
+        private readonly decimal _value;
+
+        public MoneyValue(decimal value)
+        {
+            _value = value;
+        }
+
+        public static MoneyValue Of(decimal value)
+        {
+            return new MoneyValue(value);
+        }
+
+        public static decimal operator /(MoneyValue moneyValue, int number)
+        {
+            return moneyValue._value / number;
+        }
+    }
+
     public class SalesCalculator
     {
-        public static decimal Calculate(decimal salesValue)
+        public static decimal Calculate(MoneyValue salesValue)
         {
             return salesValue / 100;
         }
